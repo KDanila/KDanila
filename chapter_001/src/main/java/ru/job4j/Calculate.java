ce4cd0e6300b104e5c585f8c4fccdfece667cebb
Danila Kuzmin <concista@gmail.com>
2017-10-10 13:07:31 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,14 @@
+package ru.job4j
+/**
+*Class Calculate задание #141
+*@author Kuzmin Danila
+*@since 10.10.2017
+*/
 public class Calculate {
+/**
+*Метод для вывода сообщения Hello World!
+*@return сообщение в консоль
+*/
     public static void main(String[] args) {
         System.out.println("Hello World!");
         }
