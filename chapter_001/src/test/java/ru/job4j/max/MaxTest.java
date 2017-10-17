c1f0cf07d7ba5b727f342a7e366596f4e38021eb
Danila Kuzmin <concista@gmail.com>
2017-10-17 11:13:16 +0300

    MaxTest without any errors!

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -16,11 +16,9 @@ public class MaxTest {
     /**
      * Test maximum of 1 and 2.
      *
-     * @param first  is first number.
-     * @param second is second number.
      */
     @Test
-    public void whenFirstLessSecond(int first, int second) {
+    public void whenFirstLessSecond() {
         Max maximum = new Max();
         int result = maximum.max(1, 2);
         assertThat(result, is(2));
@@ -29,11 +27,9 @@ public class MaxTest {
     /**
      * Test maximum of 5 and 5.
      *
-     * @param first  is first number.
-     * @param second is second number.
      */
     @Test
-    public void whenFiveandFive(int first, int second) {
+    public void whenFiveandFive() {
         Max maximum = new Max();
         int result = maximum.max(5, 5);
         assertThat(result, is(5));
@@ -42,11 +38,9 @@ public class MaxTest {
     /**
      * Test maximum of -100 and -50.
      *
-     * @param first  is first number.
-     * @param second is second number.
      */
     @Test
-    public void whenFirstIsMinusLessSecondMinus(int first, int second) {
+    public void whenFirstIsMinusLessSecondMinus() {
         Max maximum = new Max();
         int result = maximum.max(-100, -50);
         assertThat(result, is(-50));
