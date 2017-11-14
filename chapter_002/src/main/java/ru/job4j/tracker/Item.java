7943385494508dfc6c13bd95b4861248036b5ce9
Danila Kuzmin <concista@gmail.com>
2017-11-14 17:35:33 +0300

    Не до конца реализованная задача по инкапсуляции ч.2.

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,26 @@
 package main.java.ru.job4j.tracker;
 
 public class Item {
-    private static String id="0";
+    protected String name;
+    public String description;
+    public long create;
+    private String id;
 
-    public static String getId() {
-        return id;
+    public Item() {
+    }
+
+    public Item(String name, String description, long create) {
+        this.name = name;
+        this.description = description;
+        this.create = create;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setId(String id) {
--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package main.java.ru.job4j.tracker;
 
+import java.util.Random;
+
 /**
  * @version $Id$
  * @since 0.1
@@ -20,31 +22,88 @@ public class Tracker {
      *
      * @param item новая заявка
      */
+    public static final Random RN = new Random(1000);
 
+    /*
+    1. Метод public Item add(Item) добавляет заявку, переданную в аргументах в массив заявок this.items;
+     */
     public Item add(Item item) {
         item.setId(this.generateId());
         this.items[this.position++] = item;
         return item;
     }
 
+    /*
+    2. Метод public void update(Item) должен заменить ячейку в массиве this.items. Для этого необходимо найти
+     ячейку в массиве по id (id у Item можно получить с помощью метода getId). Обязательно принимать один
+     параметр Item, а не список полей;
+     */
     public void update(Item item) {
-
+        String id = item.getId();
+        for (int i = 0; i < position; i++) {
+            if (items[i].getId().equals(id)) {
+                this.items[i] = item;
+            }
+        }
     }
 
+    /* Метод public void delete(Item) должен удалить ячейку в массиве this.items. Для этого необходимо найти
+     ячейку в массиве по id. После этого присвоить ей null, либо сместить все значения справа от удаляемого
+     элемента - на одну ячейку влево с помощью System.arrayCopy();*/
     public void delete(Item item) {
+        String id = item.getId();
+        for (int i = 0; i < position; i++) {
+            if (items[i].getId().equals(id)) {
+                items[i] = null;
+                System.arraycopy(items, i, items, i + 1, position - i - 1);
+                items[position - 1] = null;
+                position--;
+            }
+        }
 
     }
 
-    public Item[] findAll(){
-        return null;
+    /*
+    4. Метод public Item[] findAll() возвращает копию массива this.items без null элементов;
+     */
+    public Item[] findAll() {
+        Item[] toReturn = new Item[this.position];
+        for (int i = 0; i != position; i++) {
+            toReturn[i] = this.items[i];
+        }
+        return toReturn;
     }
 
-    public Item[] findByName(String key){
-        return null;
+    /*
+    5. Метод public Item[] findByName(String key) проверяет в цикле все элементы массива this.items,
+    сравнивая name (используя метод getName класса Item) с аргументом метода String key. Элементы, у
+    которых совпадает name, копирует в результирующий массив и возвращает его;
+     */
+    public Item findByName(String key) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
     }
 
-    public Item findById(String id){
-        return null;
+    /*
+    6. Метод public Item findById(String id) проверяет в цикле все элементы массива this.items,
+    сравнивая id с аргументом String id и возвращает найденный Item. Если Item не найден - возвращает null.
+     */
+    public Item findById(String id) {
+
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
     }
 
     /**
@@ -54,24 +113,16 @@ public class Tracker {
      *
      * @return Уникальный ключ.
      */
-    private String generateId() {
-        String id = String.valueOf( Integer.parseInt(Item.getId()+1));
-        return id;
+    String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
     }
 }
     /*
-1. Метод public Item add(Item) добавляет заявку, переданную в аргументах в массив заявок this.items;
-2. Метод public void update(Item) должен заменить ячейку в массиве this.items. Для этого необходимо найти
- ячейку в массиве по id (id у Item можно получить с помощью метода getId). Обязательно принимать один
- параметр Item, а не список полей;
-3. Метод public void delete(Item) должен удалить ячейку в массиве this.items. Для этого необходимо найти
- ячейку в массиве по id. После этого присвоить ей null, либо сместить все значения справа от удаляемого
- элемента - на одну ячейку влево с помощью System.arrayCopy();
-4. Метод public Item[] findAll() возвращает копию массива this.items без null элементов;
-5. Метод public Item[] findByName(String key) проверяет в цикле все элементы массива this.items,
-сравнивая name (используя метод getName класса Item) с аргументом метода String key. Элементы, у
-которых совпадает name, копирует в результирующий массив и возвращает его;
-6. Метод public Item findById(String id) проверяет в цикле все элементы массива this.items,
-сравнивая id с аргументом String id и возвращает найденный Item. Если Item не найден - возвращает null.
+
+
+
+
+
+
      */
 
