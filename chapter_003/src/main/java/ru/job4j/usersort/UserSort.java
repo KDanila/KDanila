8bab592a2b1ed1e72e845ae53b1a8c316753315d
Kuzmin Danila <bus1d0@mail.ru>
2018-03-11 10:44:42 +0300

    2. Сортировка User с использованием Comparator

---
+++ chapter_003/src/main/java/ru/job4j/usersort/UserSort.java
@@ -0,0 +1,68 @@
+package ru.job4j.usersort;
+
+
+import java.util.Set;
+import java.util.List;
+import java.util.Collections;
+import java.util.TreeSet;
+import java.util.Comparator;
+
+/**
+ * UserSort class.
+ *
+ * @author Kuzmin Danila (mailto:bus1d0@mail.ru)
+ * @version $Id$
+ * @since 0.0.1
+ */
+public class UserSort {
+
+    /**
+     * Return SortedSet of users.
+     *
+     * @param users - list.
+     * @return set.
+     */
+    public Set<User> sort(List<User> users) {
+        Collections.sort(users);
+        return new TreeSet<User>(users);
+    }
+
+    /**
+     * Method return sorted List by name.
+     *
+     * @param users - users list.
+     * @return sorted list.
+     */
+
+    public List<User> sortNameLength(List<User> users) {
+        Comparator<User> nameLengthComparator = new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return (o1.getName().length()) - (o2.getName().length());
+            }
+        };
+        Collections.sort(users, nameLengthComparator);
+        return users;
+    }
+
+    /**
+     * Sjrting by two fields List<User>. First name, then age.
+     *
+     * @param users - List unsorted.
+     * @return - list after sorting.
+     */
+    public List<User> sortByAllFields(List<User> users) {
+        Comparator<User> sortByAllFieldsComparator = new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int temp = o1.getName().compareTo(o2.getName());
+                if (o1.getName().equals(o2.getName())) {
+                    temp = o1.getAge() - o2.getAge();
+                }
+                return temp;
+            }
+        };
+        Collections.sort(users, sortByAllFieldsComparator);
+        return users;
+    }
+}
--- chapter_003/src/test/java/ru/job4j/usersort/UserSortTest.java
+++ chapter_003/src/test/java/ru/job4j/usersort/UserSortTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -46,10 +45,100 @@ public class UserSortTest {
                 new User("Stanislav", 55),
                 new User("Victor", 40)
         ));
-        Collections.sort(users);
+        UserSort us = new UserSort();
+        us.sort(users);
         assertThat(users.get(0).getName(), is("Anton"));
         assertThat(users.get(1).getName(), is("Boris"));
         assertThat(users.get(2).getName(), is("Stanislav"));
         assertThat(users.get(3).getName(), is("Victor"));
     }
+
+    /**
+     * The third test.
+     */
+    @Test
+    public void thirdSortedTestwithFourElementByNameLength() {
+        ArrayList<User> users = new ArrayList<User>();
+        users.addAll(Arrays.asList(
+                new User("Bor", 30),
+                new User("Anton", 15),
+                new User("Stanislav", 55),
+                new User("Victor", 40)
+        ));
+        UserSort us = new UserSort();
+        us.sortNameLength(users);
+        assertThat(users.get(0).getName(), is("Bor"));
+        assertThat(users.get(1).getName(), is("Anton"));
+        assertThat(users.get(2).getName(), is("Victor"));
+        assertThat(users.get(3).getName(), is("Stanislav"));
+    }
+
+    /**
+     * The fourth test.
+     */
+    @Test
+    public void fourthSortedTestwithFiveElementByTwoFields() {
+        ArrayList<User> users = new ArrayList<User>();
+        users.addAll(Arrays.asList(
+                new User("Boris", 30),
+                new User("Boris", 15),
+                new User("Stanislav", 55),
+                new User("Stanislav", 40),
+                new User("Anton", 40)
+        ));
+        UserSort us = new UserSort();
+        us.sortByAllFields(users);
+        ArrayList<String> expected = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        for (User user : users) {
+            result.add(user.getName());
+            result.add(String.valueOf(user.getAge()));
+        }
+        expected.add("Anton");
+        expected.add("40");
+        expected.add("Boris");
+        expected.add("15");
+        expected.add("Boris");
+        expected.add("30");
+        expected.add("Stanislav");
+        expected.add("40");
+        expected.add("Stanislav");
+        expected.add("55");
+        assertThat(result, is(expected));
+
+    }
+
+    /**
+     * The Fifth Test.
+     */
+    @Test
+    public void fifthSortedTestwithFourElementByTwoFields() {
+        ArrayList<User> users = new ArrayList<User>();
+        users.addAll(Arrays.asList(
+                new User("Сергей", 25),
+                new User("Иван", 30),
+                new User("Сергей", 20),
+                new User("Иван", 25)
+        ));
+        UserSort us = new UserSort();
+        us.sortByAllFields(users);
+        ArrayList<String> expected = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        for (User user : users) {
+            result.add(user.getName());
+            result.add(String.valueOf(user.getAge()));
+        }
+        expected.add("Иван");
+        expected.add("25");
+        expected.add("Иван");
+        expected.add("30");
+        expected.add("Сергей");
+        expected.add("20");
+        expected.add("Сергей");
+        expected.add("25");
+        assertThat(result, is(expected));
+
+    }
+
+
 }
