76a150d3c1337b42036515eeee17b604f48bd1a2
Kuzmin Danila <bus1d0@mail.ru>
2018-03-25 21:43:03 +0300

    Отсортировать департаменты 0.1.
    Department sorting v1.1 todo JavaDock

---
+++ chapter_003/src/test/java/ru/job4j/departmentsorting/DepartmentSortingTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.departmentsorting;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DepartmentSortingTest {
+    @Test
+    public void upToDownFirstTest(){
+        //Set<String> upToDownSort(List<String> departments)
+        List<String> test = new ArrayList<>(Arrays.asList("K1\\SK1",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2\\SSSK1"));
+
+        DepartmentSorting departmentSorting = new DepartmentSorting();
+        CompareDepartmentsUp cdu = new CompareDepartmentsUp();
+        ArrayList<String> result = new ArrayList<>(departmentSorting.sort(test,cdu));
+        List<String> expected = new ArrayList<>(Arrays.asList("K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK2",
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2",
+                "K2\\SK1\\SSK2\\SSSK1"));
+        assertThat(result,is(expected));
+    }
+
+
+
+}
