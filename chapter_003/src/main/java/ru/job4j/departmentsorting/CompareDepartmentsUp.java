2e21f518596461ad6b5534eeb5ff2cc3ce93172d
Kuzmin Danila <bus1d0@mail.ru>
2018-03-26 21:50:07 +0300

    Отсортировать департаменты.

--- chapter_003/src/main/java/ru/job4j/departmentsorting/CompareDepartmentsUp.java
+++ chapter_003/src/main/java/ru/job4j/departmentsorting/CompareDepartmentsUp.java
@@ -2,7 +2,21 @@ package ru.job4j.departmentsorting;
 
 import java.util.Comparator;
 
+/**
+ * CompareDepartmentsUp class.
+ *
+ * @author Kuzmin Danila (mailto:bus1d0@mail.ru)
+ * @version $Id$
+ * @since 0.1.3
+ */
 public class CompareDepartmentsUp implements Comparator<String> {
+    /**
+     * Compare in direct direction.
+     *
+     * @param o1 -String first.
+     * @param o2 - String second.
+     * @return int.
+     */
     @Override
     public int compare(String o1, String o2) {
         return o1.compareTo(o2);
--- chapter_003/src/test/java/ru/job4j/departmentsorting/DepartmentSortingTest.java
+++ chapter_003/src/test/java/ru/job4j/departmentsorting/DepartmentSortingTest.java
@@ -5,16 +5,26 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
+
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * DepartmentSortingTest class.
+ *
+ * @author Kuzmin Danila (mailto:bus1d0@mail.ru)
+ * @version $Id$
+ * @since 0.1.3
+ */
 public class DepartmentSortingTest {
+    /**
+     * The First test.
+     */
     @Test
-    public void upToDownFirstTest(){
-        //Set<String> upToDownSort(List<String> departments)
-        List<String> test = new ArrayList<>(Arrays.asList("K1\\SK1",
+    public void upToDownTest() {
+        List<String> test = new ArrayList<>(Arrays.asList(
+                "K1\\SK1",
                 "K1\\SK2",
                 "K1\\SK1\\SSK1",
                 "K1\\SK1\\SSK2",
@@ -24,8 +34,9 @@ public class DepartmentSortingTest {
 
         DepartmentSorting departmentSorting = new DepartmentSorting();
         CompareDepartmentsUp cdu = new CompareDepartmentsUp();
-        ArrayList<String> result = new ArrayList<>(departmentSorting.sort(test,cdu));
-        List<String> expected = new ArrayList<>(Arrays.asList("K1",
+        ArrayList<String> result = new ArrayList<>(departmentSorting.sort(test, cdu));
+        List<String> expected = new ArrayList<>(Arrays.asList(
+                "K1",
                 "K1\\SK1",
                 "K1\\SK1\\SSK1",
                 "K1\\SK1\\SSK2",
@@ -35,9 +46,37 @@ public class DepartmentSortingTest {
                 "K2\\SK1\\SSK1",
                 "K2\\SK1\\SSK2",
                 "K2\\SK1\\SSK2\\SSSK1"));
-        assertThat(result,is(expected));
+        assertThat(result, is(expected));
     }
 
+    /**
+     * The Second test.
+     */
+    @Test
+    public void downToUpTest() {
+        List<String> test = new ArrayList<>(Arrays.asList(
+                "K1\\SK1",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"));
 
-
+        DepartmentSorting departmentSorting = new DepartmentSorting();
+        CompareDepartmentsDown cpd = new CompareDepartmentsDown();
+        ArrayList<String> result = new ArrayList<>(departmentSorting.sort(test, cpd));
+        List<String> expected = new ArrayList<>(Arrays.asList(
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK2",
+                "K2\\SK1\\SSK1",
+                "K1",
+                "K1\\SK2",
+                "K1\\SK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK1\\SSK1"
+        ));
+        assertThat(result, is(expected));
+    }
 }
