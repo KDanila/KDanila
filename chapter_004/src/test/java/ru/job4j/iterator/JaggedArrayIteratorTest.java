bee857fae9afa30ed6003aa4a415ebf17a46164c
Kuzmin Danila <bus1d0@mail.ru>
2018-03-31 18:37:02 +0300

    5.1.1. Итератор для двухмерного массива int

---
+++ chapter_004/src/test/java/ru/job4j/iterator/JaggedArrayIteratorTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.iterator;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * JaggedArrayIteratorTest class.
+ *
+ * @author Kuzmin Danila (mailto:bus1d0@mail.ru)
+ * @version $Id$
+ * @since 0.1.0
+ */
+public class JaggedArrayIteratorTest {
+    /**
+     * Iterator.
+     */
+    private Iterator<Integer> it;
+
+    /**
+     * Initialization.
+     */
+    @Before
+    public void setUp() {
+        it = new MatrixIterator(new int[][]{{1}, {3, 4}, {7}});
+    }
+
+    /**
+     * next() testing.
+     */
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    /**
+     * Testing next() and hasNext() without sequence.
+     */
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    /**
+     * Testing hasNext() and next().
+     */
+    @Test
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(false));
+    }
+}
