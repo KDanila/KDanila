79c5ecc7f09d01f628a98a1d33ea73d9bbfaf3d2
Kuzmin Danila <bus1d0@mail.ru>
2018-04-09 21:34:08 +0300

    0.2 Generic

--- chapter_004/src/main/java/ru/job4j/generic/RoleStore.java
+++ chapter_004/src/main/java/ru/job4j/generic/RoleStore.java
@@ -1,8 +1,65 @@
 package ru.job4j.generic;
 
-public class RoleStore extends AbstractStore {
+import ru.job4j.simplearray.SimpleArray;
 
-    protected RoleStore(int size) {
-        super(size);
+import java.util.Iterator;
+
+public class RoleStore implements Store {
+    private SimpleArray<Role> roleStoreData = new SimpleArray(10);
+
+    @Override
+    public void add(Base model) {
+        this.roleStoreData.add((Role) model);
+    }
+
+    @Override
+    public boolean replace(String id, Base model) {
+        int position = findPositionByID(id);
+        if (position == -1) {
+            this.roleStoreData.set(position, (Role) model);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        int position = findPositionByID(id);
+        if (position == -1) {
+            this.roleStoreData.delete(position);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Base findById(String id) {
+        int position = findPositionByID(id);
+        if (position == -1) {
+            return (Base) this.roleStoreData.get(position);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Find by id method if position doesn't find return -1;
+     *
+     * @param id - id in String format.
+     * @return int -position.
+     */
+    public int findPositionByID(String id) {
+        Iterator<Role> iterator =  this.roleStoreData.iterator();
+        int counter = 0;
+        while (iterator.hasNext()) {
+            Base temp = iterator.next();
+            if (temp.getId().equals(id)) {
+                return counter;
+            }
+            counter++;
+        }
+        return -1;
     }
 }
