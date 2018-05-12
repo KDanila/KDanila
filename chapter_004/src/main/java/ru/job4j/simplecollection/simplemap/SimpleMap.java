69f89022d3083ce38c205644b13cad09998a2742
Kuzmin Danila <bus1d0@mail.ru>
2018-05-12 13:25:55 +0300

    Simple Hash Set v 0.6

--- chapter_004/src/main/java/ru/job4j/simplecollection/simplemap/SimpleMap.java
+++ chapter_004/src/main/java/ru/job4j/simplecollection/simplemap/SimpleMap.java
@@ -1,20 +1,57 @@
 package ru.job4j.simplecollection.simplemap;
 
 
+
 /**
  * SimpleHashSet class.
  *
- * @param <E> - generic.
+ * @param <K> - key.
+ * @param <V> - value.
  * @author Kuzmin Danila (mailto:bus1d0@mail.ru)
  * @version $Id$
  * @since 0.1.0
  */
-public class SimpleMap<E> {
+
+public class SimpleMap<K, V> {
+    /**
+     * Insert method.
+     *
+     * @param key   - key.
+     * @param value - value.
+     * @return boolean.
+     */
+    boolean insert(K key, V value) {
+        return true;
+    }
+
+    /**
+     * Get method.
+     *
+     * @param key - key.
+     * @return V - value.
+     */
+    V get(K key) {
+        return null;
+    }
+
+    /**
+     * Delete method.
+     *
+     * @param key - key.
+     * @return boolean.
+     */
+    boolean delete(K key) {
+        return false;
+    }
 }
 /*
-Создать два объекта User, которые имеют одинаковые поля.
+Ассоциативный массив на базе хэш-таблицы должен быть унифицирован через генерики и иметь методы:
+boolean insert(K key, V value);
+V get(K key);
+boolean delete(K key);
 
-Создать карту Map<User, Object>
+Реализовывать итератор.
+Внутренняя реализация должна использовать массив. Нужно обеспечить фиксированное время вставки и получение. Предусмотрите возможность роста хэш-таблицы при нехватке места для нового элемента.
 
-Добавить два объекта. Вывести карту на печать. Описать полученный результат словами.
+Методы разрешения коллизий реализовывать не надо. Например: если при добавлении ключ уже есть, то возвращать false.
  */
