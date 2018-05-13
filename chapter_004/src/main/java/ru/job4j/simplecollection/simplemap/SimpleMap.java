1c77d8ad1949e0189d74bb0f7d729910fed22098
Kuzmin Danila <bus1d0@mail.ru>
2018-05-13 13:07:18 +0300

    Simple HashMap v 0.0

--- chapter_004/src/main/java/ru/job4j/simplecollection/simplemap/SimpleMap.java
+++ chapter_004/src/main/java/ru/job4j/simplecollection/simplemap/SimpleMap.java
@@ -1,7 +1,6 @@
 package ru.job4j.simplecollection.simplemap;
 
 
-
 /**
  * SimpleHashSet class.
  *
@@ -14,14 +13,59 @@ package ru.job4j.simplecollection.simplemap;
 
 public class SimpleMap<K, V> {
     /**
+     * keys array.
+     */
+    private Object[] keys;
+    /**
+     * values array.
+     */
+    private Object[] values;
+    /**
+     * number of elements.
+     */
+    private int size = 0;
+
+    /**
+     * Constructor.
+     */
+    public SimpleMap() {
+        this.keys = new Object[128];
+        this.values = new Object[128];
+    }
+
+    /**
+     * Overload constructor.
+     *
+     * @param size - size.
+     */
+    public SimpleMap(int size) {
+        this.keys = new Object[size];
+        this.values = new Object[size];
+    }
+
+
+    /**
      * Insert method.
+     * Не разрешаем коллизии никак.
+     * Если при добавлении ключ уже есть, то возвращать false.
      *
      * @param key   - key.
      * @param value - value.
      * @return boolean.
      */
     boolean insert(K key, V value) {
-        return true;
+        int hash = hashKey(key);
+        boolean isInsert = false;
+        if ((double) size / (double) this.keys.length >= 0.5) {
+            increaseArray();
+        }
+        if (this.keys[hash] == null) {
+            this.keys[hash] = key;
+            this.values[hash] = value;
+            isInsert = true;
+            this.size++;
+        }
+        return isInsert;
     }
 
     /**
@@ -31,7 +75,12 @@ public class SimpleMap<K, V> {
      * @return V - value.
      */
     V get(K key) {
-        return null;
+        int hash = hashKey(key);
+        Object value = null;
+        if (this.keys[hash] != null) {
+            value = this.values[hash];
+        }
+        return (V) value;
     }
 
     /**
@@ -41,8 +90,53 @@ public class SimpleMap<K, V> {
      * @return boolean.
      */
     boolean delete(K key) {
-        return false;
+        int hash = hashKey(key);
+        boolean isDelete = false;
+        if (this.keys[hash] != null) {
+            this.keys[hash] = null;
+            this.values[hash] = null;
+            isDelete = true;
+            this.size--;
+        }
+        return isDelete;
     }
+
+    /**
+     * Method increasing array.
+     */
+    private void increaseArray() {
+        int newObjectSize = this.keys.length * 2;
+        Object[] tempKeys = this.keys;
+        Object[] tempValues = this.values;
+        this.keys = new Object[newObjectSize];
+        this.values = new Object[newObjectSize];
+        for (int i = 0; i < tempKeys.length; i++) {
+            if (tempKeys[i] != null) {
+                insert((K) tempKeys[i], (V) tempValues[i]);
+                size--;
+            }
+        }
+    }
+
+    /**
+     * hashKey method. Распределяет кэш по длине массива.
+     *
+     * @param key - key.
+     * @return hash.
+     */
+    private int hashKey(K key) {
+        return key.hashCode() % this.keys.length;
+    }
+
+    /**
+     * For Simple Map test.
+     *
+     * @return size of arrays.
+     */
+    public int getSize() {
+        return this.keys.length;
+    }
+
 }
 /*
 Ассоциативный массив на базе хэш-таблицы должен быть унифицирован через генерики и иметь методы:
@@ -51,7 +145,8 @@ V get(K key);
 boolean delete(K key);
 
 Реализовывать итератор.
-Внутренняя реализация должна использовать массив. Нужно обеспечить фиксированное время вставки и получение. Предусмотрите возможность роста хэш-таблицы при нехватке места для нового элемента.
+Внутренняя реализация должна использовать массив. Нужно обеспечить фиксированное время вставки и получение.
+Предусмотрите возможность роста хэш-таблицы при нехватке места для нового элемента.
 
 Методы разрешения коллизий реализовывать не надо. Например: если при добавлении ключ уже есть, то возвращать false.
  */
