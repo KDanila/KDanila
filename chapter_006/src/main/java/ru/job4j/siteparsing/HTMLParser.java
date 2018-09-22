f89d91be33ec549276ffcdb15c03157180eac682
Danila Kuzmin <mailto:bus1d0@mail.ru>
2018-09-22 13:05:36 +0300

    XMl throw XSLT unfinished

---
+++ chapter_006/src/main/java/ru/job4j/siteparsing/HTMLParser.java
@@ -0,0 +1,6 @@
+package ru.job4j.siteparsing;
+
+public class HTMLParser {
+    //todo jsoap
+    //todo read DOM
+}
--- chapter_006/src/main/java/ru/job4j/xml/ConvertXSQT.java
+++ chapter_006/src/main/java/ru/job4j/xml/ConvertXSQT.java
@@ -1,24 +1,12 @@
 package ru.job4j.xml;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class ConvertXSQT {
 
@@ -36,6 +24,90 @@ public class ConvertXSQT {
         Source xslt = new StreamSource(scheme);
         Transformer transformer = factory.newTransformer(xslt);
         Source text = new StreamSource(source);
-        transformer.transform(text, new StreamResult(dest));
+        transformer.transform(text, new StreamResult(System.out));
+    }
+
+    public static void main(String[] args) throws TransformerException {
+        ConvertXSQT cc = new ConvertXSQT();
+        File source = new File("src\\main\\java\\ru\\job4j\\xml\\data.xml");
+        File dest = new File("src\\main\\java\\ru\\job4j\\xml\\result.xml");
+        File scheme = new File("src\\main\\java\\ru\\job4j\\xml\\template.xsl");
+        String xsl = "<?xml version=\"1.0\"?>\n" +
+                "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\n" +
+                "    <xsl:template match=\"/\">\n" +
+                "        <entries>\n" +
+                "            <xsl:for-each select=\"//entry\">\n" +
+                "                <entry>\n" +
+                "                    <xsl:attribute name=\"href\">\n" +
+                "                        <xsl:value-of select=\"sum(fields/value)\"/>\n" +
+                "                    </xsl:attribute>\n" +
+                "                </entry>\n" +
+                "            </xsl:for-each>\n" +
+                "        </entries>\n" +
+                "    </xsl:template>\n" +
+                "</xsl:stylesheet>";
+
+        String xml = "<entries>\n" +
+                "    <entry>\n" +
+                "        <fields>\n" +
+                "            <value>0</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>0</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>0</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>1</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>1</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>1</value>\n" +
+                "        </fields>\n" +
+                "    </entry>\n" +
+                "    <entry>\n" +
+                "        <fields>\n" +
+                "            <value>0</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>0</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>0</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>1</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>1</value>\n" +
+                "        </fields>\n" +
+                "        <fields>\n" +
+                "            <value>1</value>\n" +
+                "        </fields>\n" +
+                "    </entry>\n" +
+                "</entries>";
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer(
+                new StreamSource(
+                        new ByteArrayInputStream(xsl.getBytes()))
+        );
+        transformer.transform(new StreamSource(
+                        new ByteArrayInputStream(xml.getBytes())),
+                new StreamResult(System.out)
+        );
+
+        /*
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer(
+                new StreamSource(
+                        new ByteArrayInputStream(xsl.getBytes()))
+        );
+        transformer.transform(new StreamSource(
+                        new ByteArrayInputStream(xml.getBytes())),
+                new StreamResult(System.out)
+         */
     }
 }
