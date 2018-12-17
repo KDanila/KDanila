b6d71f0b7895938f68dbf2f0d7264538db4a0093
Danila Kuzmin <mailto:bus1d0@mail.ru>
2018-12-17 17:56:15 +0300

    Adding sql database store

--- chapter_007/src/main/java/ru/job4j/userservlet/DBStore.java
+++ chapter_007/src/main/java/ru/job4j/userservlet/DBStore.java
@@ -1,22 +1,59 @@
 package ru.job4j.userservlet;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 import java.sql.Connection;
 import java.sql.Statement;
+
 import org.apache.commons.dbcp2.BasicDataSource;
-//TODO sql DB store
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DBStore implements Store<User> {
+
     private static final BasicDataSource SOURCE = new BasicDataSource();
+
     private static DBStore INSTANCE = new DBStore();
 
+    private Connection connection = null;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SQLException.class);
+
+
+
     public DBStore() {
-        SOURCE.setUrl("...");
-        SOURCE.setUsername("...");
-        SOURCE.setPassword("...");
+        SOURCE.setUrl("jdbc:postgresql://localhost:5432/UsersServletDB");
+        SOURCE.setUsername("postgres");
+        SOURCE.setPassword("6799963");
         SOURCE.setMinIdle(5);
         SOURCE.setMaxIdle(10);
         SOURCE.setMaxOpenPreparedStatements(100);
+        initDataBase();
+    }
+
+    private void initDataBase() {
+        try {
+            this.connection = SOURCE.getConnection();
+        }        catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            if (this.connection == null) {
+                try {
+                    this.connection.close();
+                } catch (SQLException e) {
+                    LOGGER.error(e.getMessage(), e);
+                }
+            }
+        }
+        if (!checkDB()) {
+            createDB();
+        }
+        if (!checkTableInDB()) {
+            createTableInDB();
+        }
+    }
     }
 
     public static DBStore getInstance() {
@@ -53,6 +90,88 @@ public class DBStore implements Store<User> {
     @Override
     public User findById(int id) {
         return null;
+    }
+
+    /**
+     * createTableInDB method.
+     * <p>
+     * Creating table in database, if it's doesn't exist.
+     */
+    private void createTableInDB() {
+        Statement st = null;
+        try {
+            st = connection.createStatement();
+            //todo change this shit
+            st.executeQuery("CREATE TABLE tracker ( id integer primary key,"
+                    + "name varchar(15), "
+                    + "description varchar(50),"
+                    + "expired_date timestamp)");
+            st.close();
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * checkTableInDB method.
+     * <p>
+     * checking table in database.
+     *
+     * @return true if db existing.
+     */
+    private boolean checkTableInDB() {
+        boolean isExist = false;
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM pg_catalog.pg_tables;");
+            while (rs.next()) {
+                if (rs.getString("tablename").equals(dbTableName)) {
+                    return true;
+                }
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return isExist;
+    }
+
+    /**
+     * creatDB method.
+     */
+    private void createDB() {
+        Statement st = null;
+        try {
+            st = connection.createStatement();
+            st.executeQuery("CREATE DATABASE trackerdb");
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * checkDB method.
+     *
+     * @return true if DB is existing.
+     */
+    private boolean checkDB() {
+        boolean isExist = false;
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("SELECT datname FROM pg_database");
+            while (rs.next()) {
+                if (rs.getString("datname").equals(dbName)) {
+                    return true;
+                }
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return isExist;
     }
 }
 
